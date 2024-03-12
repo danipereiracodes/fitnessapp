35bd9fe15331f83e88fc96a23d4b9b1ca769fc0c
danipereiracodes <danipg.dev@gmail.com>
2024-03-12 23:39:11 +0100

    add alergies input and started next customers data

--- src/components/custom-checkbox/custom-checkbox.tsx
+++ src/components/custom-checkbox/custom-checkbox.tsx
@@ -1,29 +1,49 @@
+import { useEffect, useState } from 'react';
+
 interface CustomCheckboxProps {
   legend: string;
-  foodAllergies: { allergie: string }[];
+  options?: string[];
+  onFasten: () => void;
 }
 
 const CustomCheckbox: React.FC<CustomCheckboxProps> = ({
   legend,
-  foodAllergies,
+  options,
+  onFasten,
 }) => {
+  const [checkboxInput, setCheckboxInput] = useState('');
+
+  const handleCheckboxInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckboxInput(e.target.value);
+    console.log(checkboxInput);
+  };
+
+  useEffect(() => {
+    if (checkboxInput !== 'yes') return;
+    onFasten();
+  }, [checkboxInput]);
+
   return (
+    //TODO: ALOUD ONLY ONE CHECKBOX TO BE CHECKED
     <fieldset className='grid grid-cols-5 gap-2'>
       <legend>{legend}</legend>
 
-      {foodAllergies.map((element) => {
-        return (
-          <div>
-            <input
-              type='checkbox'
-              id={element.allergie}
-              name={element.allergie}
-              className='mr-2 w-[20px] h-[20px] overflow-auto'
-            />
-            <label htmlFor={element.allergie}>{element.allergie}</label>
-          </div>
-        );
-      })}
+      {options &&
+        options.map((element, index) => {
+          return (
+            <div key={index}>
+              <input
+                type='checkbox'
+                id={element}
+                name={element}
+                className='mr-2 w-[20px] h-[20px] overflow-auto'
+                value={element}
+                onChange={handleCheckboxInput}
+              />
+              <label htmlFor={element}>{element}</label>
+            </div>
+          );
+        })}
     </fieldset>
   );
 };
--- src/components/steps/step-two.tsx
+++ src/components/steps/step-two.tsx
@@ -9,37 +9,10 @@ interface StepTwoProps {
   onIsAllInputFilled: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-//TODO EXTRACT DATA
-const foodAllergies = [
-  { allergie: 'gluten' },
-  { allergie: 'lactose' },
-  { allergie: 'nuts' },
-  { allergie: 'shellfish' },
-  { allergie: 'soy' },
-  { allergie: 'eggs' },
-  { allergie: 'fish' },
-  { allergie: 'milk' },
-  { allergie: 'wheat' },
-  { allergie: 'peanuts' },
-  { allergie: 'tree nuts' },
-  { allergie: 'sesame seeds' },
-  { allergie: 'sulfites' },
-  { allergie: 'mustard' },
-  { allergie: 'celery' },
-  { allergie: 'mollusks' },
-  { allergie: 'lupin' },
-  { allergie: 'corn' },
-  { allergie: 'garlic' },
-  { allergie: 'onion' },
-  { allergie: 'citrus fruits' },
-];
-
-const StepTwo: React.FC<StepTwoProps> = ({
-  title,
-  onIsAllInputFilled,
-  step,
-}) => {
-  console.log('step 2', step);
+const StepTwo: React.FC<StepTwoProps> = ({ title, onIsAllInputFilled }) => {
+  const [textInput, setTextInput] = useState<string>('');
+  const [isFasten, setIsFasten] = useState<boolean>(false);
+  const [allergies, setAllergies] = useState<string[]>([]);
   const [userData, setUserData] = useState<{
     age: number;
     height: number;
@@ -53,6 +26,41 @@ const StepTwo: React.FC<StepTwoProps> = ({
       setUserData(JSON.parse(prevData ?? ''));
     }
   }, []);
+
+  let splittedText: string[];
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTextInput(e.target.value);
+    splittedText = textInput.split(',');
+    console.log(splittedText);
+  };
+
+  const handleBlur = () => {
+    const splittedText = textInput.split(/[,. ]+/);
+    setAllergies((prevAllergies) => [
+      ...new Set([...prevAllergies, ...splittedText]),
+    ]);
+    console.log(allergies);
+  };
+
+  const onHandleFasten = () => {
+    setIsFasten(!isFasten);
+  };
+
+  const fastingDialogueOpen = () => {
+    return (
+      <div>
+        <CustomSelect
+          label={'How often do you do it?'}
+          options={['12 hours', '14 hours', '16hours']}
+          onSelect={() => {}}
+          onHandleAllInputs={() => {}}
+          value=''
+        />
+      </div>
+    );
+  };
+
   return (
     <section className="relative flex flex-col w-full h-full justify-center items-center p-4  bg-[url('/image/fitness_food_background.avif')] bg-center">
       <div className='absolute w-full h-full inset-0 bg-[rgba(0,0,0,0.41)]'></div>
@@ -69,10 +77,27 @@ const StepTwo: React.FC<StepTwoProps> = ({
                 value=''
               />
 
-              <CustomCheckbox
-                legend='Do you have alergies?'
-                foodAllergies={foodAllergies}
+              <label htmlFor='alergies'>
+                Any alergies? (add them separated by comas, dots or spaces.
+                Example: "gluten,lactose etc..")
+              </label>
+              <input
+                className='text-black'
+                type='text'
+                name='alergies'
+                value={textInput}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
+              {allergies?.map((item) => {
+                return <h3>{item}</h3>;
+              })}
+              <CustomCheckbox
+                legend='Do you practice intermittent fasting?'
+                options={['yes', 'no', 'dunno']}
+                onFasten={onHandleFasten}
+              />
+              {isFasten && fastingDialogueOpen()}
             </div>
           </div>
           <div className='flex flex-col items-center w-1/3 border-l border-black'>
